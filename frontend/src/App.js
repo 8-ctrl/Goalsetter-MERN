80770c6788cb75efb5e4513308b40c9f3b77aaf0
Neel <neelpatel1029@gmail.com>
2022-03-14 16:37:30 +0530

    Login and Register Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,26 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Header from './components/Header';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import{register, reset} from '../src/features/auth/authSlice.js'
 
 function App() {
   return (
     <>
     <Router>
     <div className='container'>
-      <h1>My App</h1>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Dashboard/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
     </div>
     </Router>
+    <ToastContainer/>
     </>
   );
 }
